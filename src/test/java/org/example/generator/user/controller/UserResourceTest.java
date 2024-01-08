3f41ca9332cce94da99dc1eea58839181130835b
sycho <csyull2287@gmail.com>
2024-01-08 23:51:21 +0900

    fix

--- src/test/java/org/example/generator/user/controller/UserResourceTest.java
+++ src/test/java/org/example/generator/user/controller/UserResourceTest.java
@@ -2,11 +2,13 @@ package org.example.generator.user.controller;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
+@Slf4j
 class UserResourceTest {
 
     private final String baseUrl = "http://localhost:8080";
@@ -18,12 +20,9 @@ class UserResourceTest {
         String url = baseUrl + "/users";
         ResponseEntity<String> responseEntity = restTemplate.getForEntity(url, String.class);
 
-        // HTTP 응답 상태가 200 OK 여부 확인
         assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
 
-        // 실제로는 JSON 응답 바디를 파싱하여 테스트하게 될 것입니다.
-        // 여기서는 간단히 응답 문자열을 출력해 봅니다.
-        System.out.println("Response body: " + responseEntity.getBody());
+        log.debug("Response body: " + responseEntity.getBody());
     }
 
     @Test
@@ -33,11 +32,8 @@ class UserResourceTest {
 
         ResponseEntity<String> responseEntity = restTemplate.getForEntity(url, String.class, userId);
 
-        // HTTP 응답 상태가 200 OK 여부 확인
         assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
 
-        // 실제로는 JSON 응답 바디를 파싱하여 테스트하게 될 것입니다.
-        // 여기서는 간단히 응답 문자열을 출력해 봅니다.
-        System.out.println("Response body: " + responseEntity.getBody());
+        log.debug("Response body: " + responseEntity.getBody());
     }
 }
